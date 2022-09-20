da26207270d970e4a9343da54769ec83e4312fd7
Babatunde <babs20001@gmail.com>
2022-09-19 18:44:19 -0700

    corections made to remove trailing whitespace for task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the value stored in a and b 
+ * swap_int - swaps the value stored in a and b
  * @a: first value to swap for b
  * @b: second value to swap for a
  * Return: returns void on success
