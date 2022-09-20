db331ba903a8d55ebe3888646cc613517be8f78a
Babatunde <babs20001@gmail.com>
2022-09-19 18:31:08 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets any number to 98 with pointer
+ * @n: is the pointer to use
+ * Return:returns void on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
