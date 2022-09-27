ab01e8fe568936c41781dda338b8819f63bdccea
Babatunde <babs20001@gmail.com>
2022-09-27 07:13:55 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _memset - fills memory with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: bytes filled.
+ * @s: memory area,
+ * @b: constant byte,
+ * @n: bytes filled,
  * Return: the pointer to destination.
  */
 char *_memset(char *s, char b, unsigned int n)
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifndef _MAIN_H_
+#ifndef _MAIN_He
 #define _MAIN_H_
 
 char *_memset(char *s, char b, unsigned int n);
@@ -7,9 +7,8 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _putchar(char);
 void print_chessboard(char (*a)[8];
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int _putchar(char);
-
 #endif
