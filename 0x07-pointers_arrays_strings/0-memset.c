8e47630012e8b2ddc183c7b144594d95494fb7f7
Babatunde <babs20001@gmail.com>
2022-09-27 02:39:29 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * @s: memory area.
  * @b: constant byte.
  * @n: bytes filled.
- * Return: the pointer to dest>
+ * Return: the pointer to dest.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
