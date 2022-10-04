fa5c48ba2fc3ef4e21d6ceffc3113e72bf567a9f
Babatunde <babs20001@gmail.com>
2022-10-04 07:02:39 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabets in lowecase.
+ *
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	_putchar(ch);
+	_putchar('\n');
+}
