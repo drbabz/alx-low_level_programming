e599ecd475c2aaff7807aa5033b200de13639ab6
Babatunde <babs20001@gmail.com>
2022-10-04 01:57:21 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the name followed by a new line 
- * @argc: number of command line arguments 
+ * main - prints the name followed by a new line
+ * @argc: number of command line arguments
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
